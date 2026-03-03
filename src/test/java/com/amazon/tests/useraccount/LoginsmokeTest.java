57b000bd016aef7318299a5c77a2b58acec13d8d
pypro6699-gif <your_new_email@example.com>
2026-03-03 08:41:59 +0530

    renamed login smoke

--- src/test/java/com/amazon/tests/useraccount/LoginsmokeTest.java
+++ src/test/java/com/amazon/tests/useraccount/LoginsmokeTest.java
@@ -5,7 +5,7 @@ import com.amazon.pages.LoginPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Login_smoke extends BaseTest {
+public class LoginsmokeTest extends BaseTest {
     String phoneNum="7259356897";
     String password = "SAIBABA123";
 
@@ -15,7 +15,7 @@ public class Login_smoke extends BaseTest {
     public void verifyUserCanLoginWithValidCreds() throws InterruptedException {
        // m=3;
         //System.out.println(m);
-        m2();
+       // m2();
         LoginPage lp = new LoginPage(driver);
         lp.clickSignIn();
         lp.enterMailORNum(phoneNum);
